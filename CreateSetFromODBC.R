839c04563f9d9cc0a8b329a8809ff95de87a4906
Matteo Castagna <matteo.castagna@gmail.com>
2015-05-22 16:08:28 +0100

    Added ODBC source

---
+++ CreateSetFromODBC.R
@@ -0,0 +1,15 @@
+library(RODBC)
+
+rm(list =ls(all=TRUE))
+
+channel <- odbcConnect("SQLServerPerfRep")
+
+
+PerfAll<- as.data.frame(sqlQuery(channel, "EXEC spS_GetCEOPerfAll '2015-04-30', null, 1"))
+PerfDesk<- as.data.frame(sqlQuery(channel, "EXEC spS_GetCEOPerfDesk '2015-04-30', null, 1"))
+
+close(channel)
+
+save(PerfAll, file = "PerfAll.Rda")
+save(PerfDesk, file = "PerfDesk.Rda")
+
